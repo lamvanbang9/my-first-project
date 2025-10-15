ba838a1206eafe92e556381ae1d98404320c30ff
lamvanbang9 <lamvanbang25@gmail.com>
2025-10-15 16:14:36 +0700

    feat(cli): allow passing name via command line

--- main.py
+++ main.py
@@ -1,1 +1,1 @@
-'print("Hello from My First Project!")' 
+'import sys\nfrom greet import greet\n\nif __name__ == "__main__":\n    name = sys.argv[1] if len(sys.argv)  else ""\n    print(greet(name))' 
